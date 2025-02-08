7f9ff776d638b876200f9bcb2c6ca881a9309a70
Rivky <rivka747343@gmail.com>
2025-02-09 01:19:24 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const axios = require('axios');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/apps', async (req, res) => {
+    try {
+        const response = await axios.get('https://api.render.com/v1/services', {
+            headers: {
+                'Authorization': `Bearer ${process.env.RENDER_API_KEY}`
+            }
+        });
+        res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error retrieving applications');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
